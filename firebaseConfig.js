34f22db86520ccb9de9660b321f3efb93d08632e
salemaziel <mymainemail0501@gmail.com>
2020-08-06 15:25:41 -0700

    updates

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,9 +1,8 @@
 module.exports = {
-  apiKey: '#',
-  authDomain: '#',
-  databaseURL: '#',
-  projectId: '#',
-  storageBucket: '#',
-  messagingSenderId: '#',
-  appId: '#',
+  apiKey: process.env.GATSBY_FIREBASE_API_KEY,
+  authDomain: process.env.GATSBY_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.GATSBY_FIREBASE_DATABASE_URL,
+  projectId: process.env.GATSBY_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.GATSBY_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.GATSBY_FIREBASE_MESSAGING_SENDER_ID,
 };
